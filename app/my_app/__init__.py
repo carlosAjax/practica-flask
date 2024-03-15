ea95d5678ed423455a6d8d5f605d584443502065
ajax <ajaxajaxcarlos@gmail.com>
2024-03-15 02:14:07 -0600

    Proyecto base

---
+++ app/my_app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from my_app.tasks.controllers import taskRoute
+from my_app.config import DevConfig
+
+app = Flask(__name__)
+app.register_blueprint(taskRoute)
+app.config.from_object(DevConfig)
